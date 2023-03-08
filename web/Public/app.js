9c41f1dc448f4aa47bce068ff3f041c78185e6fa
Sanjiv1458 <sanjiv1458@gmail.com>
2023-03-08 16:13:42 +0530

    new: all files for topic3

---
+++ web/Public/app.js
@@ -0,0 +1,26 @@
+$('#navbar').load('navbar.html');
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+
+
+devices.forEach(function (device) {
+  $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+      </tr>`
+  );
+});
+
+$('#add-device').on('click', function () {
+  const user = $('#user').val();
+  const name = $('#name').val();
+  devices.push({ user, name });
+  localStorage.setItem('devices', JSON.stringify(devices));
+  location.href = 'device-list.html';
+});
+
+$('#reset-data').on('click', function () {
+  localStorage.clear();
+  location.href = 'device-list.html';
+});
